bd82733f8003a3bf1bf1d3af8a0881d9ac69cd85
kimjeonghyun <rlawjdgus051@naver.com>
2021-03-22 21:54:36 +0900

    2. 인스타그램 클론 - 시큐리티 설정완료

---
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.cos.costagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 모델 : image, User, Likes, Follow, Tag : 인증 필요함
+	// Auth 주소 : 인증 필요없음.
+	// static  폴더
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+		//http.cors().disable(); // 컨트롤러에 CrossOrigin을 붙여도, 시큐리티에서 막힘
+		http.authorizeRequests()
+			.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')") //막을것만 막고 나머진 허용 / authenticated:로그인만하면 허용 /
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest()
+			.permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/");
+			// OAuth2.0과 CORS는 나중에!!
+		
+	}
+}
